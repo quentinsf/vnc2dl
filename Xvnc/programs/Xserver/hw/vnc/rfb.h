d184eadc9e02e3d5ca99cb13af2a217d045b513a
const <const@3789f03b-4d11-0410-bbf8-ca57d06f2519>
2000-08-18 05:12:57 +0000

    Constants moved back to header file.
    git-svn-id: https://vnc-tight.svn.sourceforge.net/svnroot/vnc-tight/orig/trunk/vnc_unixsrc@701 3789f03b-4d11-0410-bbf8-ca57d06f2519

--- Xvnc/programs/Xserver/hw/vnc/rfb.h
+++ Xvnc/programs/Xserver/hw/vnc/rfb.h
@@ -450,6 +450,9 @@ extern Bool rfbSendRectEncodingHextile(rfbClientPtr cl, int x, int y, int w,
 
 /* tight.c */
 
+#define TIGHT_MAX_RECT_WIDTH 512
+#define TIGHT_MAX_RECT_HEIGHT 128
+
 extern Bool rfbSendRectEncodingTight(rfbClientPtr cl, int x,int y,int w,int h);
 
 
