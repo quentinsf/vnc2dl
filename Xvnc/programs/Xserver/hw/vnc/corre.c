244860ec99cbea15cc3b457d727c1c09079f4c22
const <const@3789f03b-4d11-0410-bbf8-ca57d06f2519>
2001-01-16 22:26:22 +0000

    Bug fixed: CoRRE implementation refused to check return codes after writing data to clients; this caused Xvnc crashes after write operation into closed descriptor.
    git-svn-id: https://vnc-tight.svn.sourceforge.net/svnroot/vnc-tight/orig/trunk/vnc_unixsrc@811 3789f03b-4d11-0410-bbf8-ca57d06f2519

--- Xvnc/programs/Xserver/hw/vnc/corre.c
+++ Xvnc/programs/Xserver/hw/vnc/corre.c
@@ -60,20 +60,18 @@ rfbSendRectEncodingCoRRE(cl, x, y, w, h)
     int x, y, w, h;
 {
     if (h > cl->correMaxHeight) {
-	rfbSendRectEncodingCoRRE(cl, x, y, w, cl->correMaxHeight  );
-	rfbSendRectEncodingCoRRE(cl, x, y + cl->correMaxHeight, w,
-				 h - cl->correMaxHeight);
-	return;
+	return (rfbSendRectEncodingCoRRE(cl, x, y, w, cl->correMaxHeight) &&
+		rfbSendRectEncodingCoRRE(cl, x, y + cl->correMaxHeight,
+					 w, h - cl->correMaxHeight));
     }
 
     if (w > cl->correMaxWidth) {
-	rfbSendRectEncodingCoRRE(cl, x, y, cl->correMaxWidth, h);
-	rfbSendRectEncodingCoRRE(cl, x + cl->correMaxWidth, y,
-				 w - cl->correMaxWidth, h);
-	return;
+	return (rfbSendRectEncodingCoRRE(cl, x, y, cl->correMaxWidth, h) &&
+		rfbSendRectEncodingCoRRE(cl, x + cl->correMaxWidth, y,
+					 w - cl->correMaxWidth, h));
     }
 
-    rfbSendSmallRectEncodingCoRRE(cl, x, y, w, h);
+    return rfbSendSmallRectEncodingCoRRE(cl, x, y, w, h);
 }
 
 
