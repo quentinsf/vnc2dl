1b75053e7de6842233d5b2b3566c5604b7546634
const <const@3789f03b-4d11-0410-bbf8-ca57d06f2519>
2002-03-20 05:16:03 +0000

    Applied vnc-imake patch from RedHat vnc RPM. This should add support for more architechtures under Linux.
    git-svn-id: https://vnc-tight.svn.sourceforge.net/svnroot/vnc-tight/orig/trunk/vnc_unixsrc@1050 3789f03b-4d11-0410-bbf8-ca57d06f2519

--- Xvnc/config/imake/imakemdep.h
+++ Xvnc/config/imake/imakemdep.h
@@ -294,9 +294,15 @@ char *cpp_argv[ARGUMENTS] = {
 #ifdef unix
 	"-Uunix",	/* remove unix symbol so that filename unix.c okay */
 #endif
-#if defined(__386BSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__FreeBSD__) || defined(MACH)
+#if defined(__386BSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__FreeBSD__) || defined(MACH) || defined(linux) || defined(__GNU__)
 # ifdef __i386__
 	"-D__i386__",
+# endif
+# ifdef __sparc__
+	"-D__sparc__",
+# endif
+# ifdef __alpha__
+       "-D__alpha__",
 # endif
 # ifdef __GNUC__
 	"-traditional",
